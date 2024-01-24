1ccd6c5d2f7b4e35d75e86ccee1bf99289968418
ajay sonere <ajaysonere786@gmail.com>
2024-01-24 12:04:51 +0530

    print pairs

---
+++ 06-Arrays/PairsInArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class PairsInArray {
+  
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    
+    for(int i=0; i<arr.length; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    //print pairs in array 
+    
+    for(int i=0; i<arr.length-1; i++){
+      for(int j=i+1; j<arr.length; j++){
+        System.out.print( "(" + arr[i] + " " + arr[j] + ") ,");
+      }
+      System.out.println();
+    }
+  }
+}
